bfa2cfed8fb9c5f140528c7fe1a4c134f031e234
igorjosiel <igorjosielvitalino@gmail.com>
2026-08-16 18:45:46 -0300

    feat: estiliza os cards dos eventos.

--- src/components/CardEvent/index.jsx
+++ src/components/CardEvent/index.jsx
@@ -5,14 +5,14 @@ export function CardEvent({ evento }) {
         <div className='card-event'>
             <img src={evento.capa} alt={evento.titulo}/>
             
-            <div className="corpo">
+            <div className="body">
                 <p className="tag">
                     {evento.tema.nome}
                 </p>
-                <p>
+                <p className='date'>
                     {evento.data.toLocaleDateString('pt-BR')}
                 </p>
-                <h4 className="titulo">
+                <h4 className="title">
                     {evento.titulo}
                 </h4>
             </div>
